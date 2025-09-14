a2e84a8a016e4778f7b5610a97b40223c9308784
rabbil786 <hantingbangla@gmail.com>
2025-09-14 06:38:29 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E')
+bot = telebot.TeleBot('7595804649:AAGA9QSbiKML1gM9vUZx42VTkf0gP6W2LIk')
 
 # Admin user IDs
-admin_id = ["5588464519"]
+admin_id = ["7657591035"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -451,5 +451,6 @@ while True:
         bot.polling(none_stop=True)
     except Exception as e:
         print(e)
+
 
 
